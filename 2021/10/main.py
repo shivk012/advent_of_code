ce5bbff3af10b1d379edf12bd1cca1fb17d554e6
Shivam <shivam_k@hotmail.com>
2022-01-01 16:10:02 +0000

    Working day 10

--- 2021/10/main.py
+++ 2021/10/main.py
@@ -1,25 +1,52 @@
-
 import os
 import unittest
+
+POINTS = {")": 3, "]": 57, "}": 1197, ">": 25137}
+OPENER = {")": "(", "]": "[", "}": "{", ">": "<"}
+POINTS_2 = {")": 1, "]": 2, "}": 3, ">": 4}
+CLOSER = {"(": ")", "[": "]", "{": "}", "<": ">"}
 
 def read_data():
     dir = os.path.dirname(os.path.realpath(__file__))
     input_file = os.path.join(dir, "input.txt")
 
-    with open(input_file, 'r') as f:
+    with open(input_file, "r") as f:
         data = f.readlines()
-        
+
     data = list(map(lambda x: x.strip(), data))
-    
+
     return data
 
 
 def part_1(data):
-    pass
+    score = 0
+    for line in data:
+        stack = []
+        for char in line:
+            if char in "({[<":
+                stack.append(char)
+            elif stack.pop() != OPENER[char]:
+                score += POINTS[char]
+                break
+    return score
 
 
 def part_2(data):
-    pass
+    scores = []
+    for line in data:
+        stack = []
+        score = 0
+        for c in line:
+            if c in CLOSER:
+                stack.append(c)
+            elif stack.pop() != OPENER[c]:
+                break
+        else:
+            while len(stack) > 0:
+                c = CLOSER[stack.pop()]
+                score = score * 5 + POINTS_2[c]
+            scores.append(score)
+    return sorted(scores)[len(scores) // 2]
 
 
 def main():
@@ -29,25 +56,26 @@ def main():
 
 
 class Test(unittest.TestCase):
-    test_data = '''[({(<(())[]>[[{[]{<()<>>
-[(()[<>])]({[<{<<[]>>(
-{([(<{}[<>[]}>{[]{[(<()>
-(((({<>}<{<{<>}{[]{[]{}
-[[<[([]))<([[{}[[()]]]
-[{[{({}]{}}([{[{{{}}([]
-{<[[]]>}<{[{[{[]{()[[[]
-[<(<(<(<{}))><([]([]()
-<{([([[(<>()){}]>(<<{{
-<{([{{}}[<[[[<>{}]]]>[]]
-'''
+    test_data = """[({(<(())[]>[[{[]{<()<>>
+                [(()[<>])]({[<{<<[]>>(
+                {([(<{}[<>[]}>{[]{[(<()>
+                (((({<>}<{<{<>}{[]{[]{}
+                [[<[([]))<([[{}[[()]]]
+                [{[{({}]{}}([{[{{{}}([]
+                {<[[]]>}<{[{[{[]{()[[[]
+                [<(<(<(<{}))><([]([]()
+                <{([([[(<>()){}]>(<<{{
+                <{([{{}}[<[[[<>{}]]]>[]]"""
     test_data = test_data.split("\n")
+    test_data = [x.strip() for x in test_data]
 
     def test_1(self):
-        self.assertEqual(part_1(self.test_data), 7)
+        self.assertEqual(part_1(self.test_data), 26397)
 
     def test_2(self):
-        self.assertEqual(part_2(self.test_data), 5)
+        self.assertEqual(part_2(self.test_data), 288957)
+
 
 if __name__ == "__main__":
-    unittest.main()
-    #main()
+    # unittest.main()
+    main()
