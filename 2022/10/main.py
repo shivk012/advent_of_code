d29d896048f1041d46c8a5ca2b4ac955237fd6f6
Shivam Kumar <shivam.kumar@kraken.tech>
2022-12-18 12:16:09 +0000

    Day 9 and wip day 10

--- 2022/10/main.py
+++ 2022/10/main.py
@@ -15,9 +15,23 @@ def read_data():
 
 
 def part_1(data):
-    pass
+    cycles = [1]
 
-
+    for instruction in data:
+        print(f"cycles_count: {len(cycles)=}")
+        print(instruction)
+        match instruction.split(" "):
+            case ["noop"]: 
+                cycles.append(cycles[-1])
+            case ["addx", x]:
+                cycles.append(cycles[-1])
+                cycles.append(cycles[-1] + int(x))
+        print(cycles)
+    output = 0
+    for x in [20, 60, 100, 140, 180, 220]:
+        print(f"{x=}: {cycles[x]=} {x*cycles[x]=}")
+        output += x*cycles[x]
+    return output
 def part_2(data):
     pass
 
@@ -29,14 +43,157 @@ def main():
 
 
 class Test(unittest.TestCase):
-    test_data = '''noop
+    test_data = '''addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
 addx 3
-addx -5
-'''
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop'''
+
     test_data = test_data.split("\n")
 
     def test_1(self):
-        self.assertEqual(part_1(self.test_data), 7)
+        self.assertEqual(part_1(self.test_data), 13140)
 
     def test_2(self):
         self.assertEqual(part_2(self.test_data), 5)
--- 2022/9/main.py
+++ 2022/9/main.py
@@ -15,11 +15,45 @@ def read_data():
 
 
 def part_1(data):
-    pass
+    # treat grid as a complex plane
+    head = 0+0j
+    tail = 0+0j
+    directional_movement = {'R':+1, 'L':-1, 'U':1j, 'D':-1j}
+    seen = {tail}
+    tail_movement = lambda x: complex((x.real>0) - (x.real<0), (x.imag>0) - (x.imag<0))
+    
+    for line in data:
+        direction = line[0]
+        steps = int(line[2:])
 
+        for i in range(steps):
+            head += directional_movement[direction]
+            # needs to be 2 so that a diagonal separation doesn't count as 1
+            if abs(head-tail) >=2 :
+                tail += tail_movement(head-tail)
+                seen.add(tail)
+    return len(seen)
 
 def part_2(data):
-    pass
+    rope = [0+0j] * 10
+    directional_movement = {'R':+1, 'L':-1, 'U':1j, 'D':-1j}
+    seen = [set([x]) for x in rope]
+    tail_movement = lambda x: complex((x.real>0) - (x.real<0), (x.imag>0) - (x.imag<0))
+    
+    for line in data:
+        direction = line[0]
+        steps = int(line[2:])
+
+        for _ in range(steps):
+            rope[0] += directional_movement[direction]
+            # needs to be 2 so that a diagonal separation doesn't count as 1
+            for i in range(1,10):
+                dist = rope[i-1]-rope[i]
+                if abs(dist) >=2 :
+                    rope[i] += tail_movement(dist)
+                    seen[i].add(rope[i])
+
+    return len(seen[9])
 
 
 def main():
@@ -29,27 +63,35 @@ def main():
 
 
 class Test(unittest.TestCase):
-    test_data = '''....
-.TH.
-....
+    test_data = '''R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2'''
 
-....
-.H..
-..T.
-....
+    test_data_2 = '''R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20'''
 
-...
-.H. (H covers T)
-...
-'''
     test_data = test_data.split("\n")
-
+    test_data_2 = test_data_2.split("\n")
+    
     def test_1(self):
-        self.assertEqual(part_1(self.test_data), 7)
+        self.assertEqual(part_1(self.test_data), 13)
 
     def test_2(self):
-        self.assertEqual(part_2(self.test_data), 5)
+        self.assertEqual(part_2(self.test_data), 1)
 
+    def test_3(self):
+        self.assertEqual(part_2(self.test_data_2), 36)
 if __name__ == "__main__":
-    unittest.main()
-    #main()
+    # unittest.main()
+    main()
